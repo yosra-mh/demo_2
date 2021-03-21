0682fdcde1c1047f4d020a1fa43496926c148aff
yosra_mhiri <yosraamhiri@gmail.com>
2021-03-21 12:10:08 +0100

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import react from 'react';
 
-
-const App = () => {
-  const now = new Date();
-  const a =10;
-  const b =20;
-  console.log("hello from another world ")
-  console.log("Now:", now);
-  return (
+const Hello = () =>{
+  return(
     <div>
-      <p>Hello World, it's {now.toString()} !</p>
-      <p> {a} + {b} = {a+b} </p>
-      
+      <p> Hello world !</p>
     </div>
   )
+}
+
+
+const App = () => {
+  
+  return (
+    <div> 
+      <h1>Greeting</h1> 
+      <Hello/>   
+      <Hello/>       
+    </div>
+  ) 
 }
 
 export default App;
