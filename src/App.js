5ed57dfaf40b445d92c33e68b2eb31f9131bb73e
yosra_mhiri <yosraamhiri@gmail.com>
2021-03-21 12:15:48 +0100

    props passing data to components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import react from 'react';
 
-const Hello = () =>{
+const Hello = (props) =>{
   return(
     <div>
-      <p> Hello world !</p>
+      <p> Hello {props.name}! you are {props.age} years old.</p>
     </div>
   )
 }
 
+const Footer =() => {
+  const today = new Date() 
+  return (
+    <div>
+     <em>Hello app created</em> {today.toString()}
+    </div>
+  )
+}
 
 const App = () => {
+  const name = 'aladdin';
+  const age = 25;
   
   return (
     <div> 
       <h1>Greeting</h1> 
-      <Hello/>   
-      <Hello/>       
+      <Hello name="Mohamed" age={26+10}/>   
+      <Hello name={name} age={age} /> 
+      <Footer />      
     </div>
   ) 
 }
