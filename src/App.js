e764468bce2b209fc201de664e6263cfad71617f
yosra_mhiri <yosraamhiri@gmail.com>
2021-03-21 12:01:13 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
 
-function App() {
+
+const App = () => {
+  const now = new Date();
+  const a =10;
+  const b =20;
+  console.log("hello from another world ")
+  console.log("Now:", now);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World, it's {now.toString()} !</p>
+      <p> {a} + {b} = {a+b} </p>
+      
     </div>
-  );
+  )
 }
 
 export default App;
